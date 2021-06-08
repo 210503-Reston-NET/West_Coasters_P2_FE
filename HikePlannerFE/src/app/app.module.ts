e7a472876b53c677263db4437313baafb2a427d8
sehar-malik <sehar.malik508@gmail.com>
2021-06-08 12:17:36 -0700

    added header footer, fixed animation bug and added little bit style

--- HikePlannerFE/src/app/app.module.ts
+++ HikePlannerFE/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SearchTrailComponent } from './trails-map/search-trail/search-trail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    SearchTrailComponent
+    SearchTrailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
