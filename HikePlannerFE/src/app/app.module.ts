aa5c04b0772b7f5022ef1d3b96087c860a744edf
sehar-malik <sehar.malik508@gmail.com>
2021-06-09 07:04:43 -0700

    implemented simple Auth0 Authentication

--- HikePlannerFE/src/app/app.module.ts
+++ HikePlannerFE/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//Import Auth0 sdk
+import { AuthModule } from '@auth0/auth0-angular';
+import { AuthButtonComponentComponent } from './auth-button-component/auth-button-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthButtonComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule.forRoot({
+      domain: 'dev-c74jvj1k.us.auth0.com',
+      clientId: '2ekjl6sZMK3vjFWpTa8ngr1G66fHpJpE'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ HikePlannerFE/src/app/auth-button-component/auth-button-component.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
+@Component({
+  selector: 'app-auth-button',
+  template: `
+  <ng-container *ngIf="auth.isAuthenticated$ | async; else loggedOut">
+    <button (click)="auth.logout({ returnTo: document.location.origin })">
+      Log out
+    </button>
+  </ng-container>
+
+  <ng-template #loggedOut>
+    <button (click)="auth.loginWithRedirect()">Log in</button>
+  </ng-template>
+`,
+})
+export class AuthButtonComponentComponent implements OnInit {
+
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
+
+  ngOnInit(): void {
+  }
+
+}
