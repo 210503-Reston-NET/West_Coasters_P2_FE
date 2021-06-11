65db2833d2c4aa17c2b77b08ca3fa01b9c125644
Phoebe <jia.520@outlook.com>
2021-06-11 10:03:28 -0400

    added compoent for checklist

---
+++ HikePlannerFE/src/app/checklist/add-checklist/add-checklist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-checklist',
+  templateUrl: './add-checklist.component.html',
+  styleUrls: ['./add-checklist.component.css']
+})
+export class AddChecklistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
