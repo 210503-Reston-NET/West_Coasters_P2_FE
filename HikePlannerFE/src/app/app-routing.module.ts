902d807633db4a181b56e6cf70d38dc1302d2a19
sehar-malik <sehar.malik508@gmail.com>
2021-06-10 18:55:54 -0700

    login/logout functional and some routing changes done

--- HikePlannerFE/src/app/app-routing.module.ts
+++ HikePlannerFE/src/app/app-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddEquipmentComponent } from './equipments/add-equipment/add-equipment.component';
 import { GetEquipmentsComponent } from './equipments/get-equipments/get-equipments.component';
+import { MapComponent } from './map/map.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   {
     path: 'equipments',
     component: GetEquipmentsComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'trail',
+    component: MapComponent
   },
   {
     path: 'addEquipment',
--- HikePlannerFE/src/app/app.module.ts
+++ HikePlannerFE/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { AddEquipmentComponent } from './equipments/add-equipment/add-equipment.
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { AuthModule } from '@auth0/auth0-angular';
+import { AuthButtonComponent } from './auth-button/auth-button.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -18,14 +21,20 @@ import { FormsModule } from '@angular/forms';
     FooterComponent,
     MapComponent,
     GetEquipmentsComponent,
-    AddEquipmentComponent
+    AddEquipmentComponent,
+    AuthButtonComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     TrailsMapModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AuthModule.forRoot({
+      domain: 'dev-c74jvj1k.us.auth0.com',
+      clientId: '2ekjl6sZMK3vjFWpTa8ngr1G66fHpJpE'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
