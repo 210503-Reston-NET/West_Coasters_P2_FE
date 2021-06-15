27f1673eaf338935e38d533269c0ed4f827d6d36
sehar-malik <sehar.malik508@gmail.com>
2021-06-14 20:44:44 -0700

    wrote fetch shared trips function in creatortrip file

--- HikePlannerFE/src/app/trips/creatortrips/creatortrips.component.ts
+++ HikePlannerFE/src/app/trips/creatortrips/creatortrips.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { trip } from 'src/app/models/trip';
 import { trips } from 'src/app/models/trips';
 import { HPApiService } from 'src/app/services/hpapi.service';
 
@@ -10,6 +11,7 @@ import { HPApiService } from 'src/app/services/hpapi.service';
 })
 export class CreatortripsComponent implements OnInit {
   trips: trips[] = [];
+  tripInvite : trip[] = [];
   constructor(private hpApi: HPApiService, private router: Router) { }
   currentUserId = window.sessionStorage.getItem('currentUserId') ?? ''
 
@@ -17,6 +19,16 @@ export class CreatortripsComponent implements OnInit {
     this.hpApi.GetTripsByCreator(this.currentUserId).then(
       result => this.trips = result
       )
+      this.hpApi.GetSharedTrips(this.currentUserId).then(
+        result => {
+          result.filter(r => r.participants?.filter(p => p.accept == false))
+          this.tripInvite = result;
+         // console.log(currentUserId);
+          console.log("tripInvite -> ",result);
+          console.log("tripInvite -> ",this.tripInvite);
+  
+        }
+      );
     }
     TripDetails(tripId: number,activitytrailId: number, activityId: number) : void {
       this.router.navigate(['tripdetails'], {queryParams:{id: tripId, activitytrailId: activitytrailId, activityId: activityId}});
