d15f4e5c3c4ac40b31ca7769f09b1cd9e7ae355d
Juniper Song <songminseonk@gmail.com>
2021-06-14 21:01:35 -0700

    back to prod backend..

--- HikePlannerFE/src/app/services/hpapi.service.ts
+++ HikePlannerFE/src/app/services/hpapi.service.ts
@@ -14,9 +14,7 @@ import { participant } from '../models/participant';
   providedIn: 'root'
 })
 export class HPApiService {
-  // baseURL: string = 'https://hikeplannerapi.azurewebsites.net/api/';
-  baseURL: string = 'https://hikeplannerrest.azurewebsites.net/api/';
-  // baseURL: string = 'http://localhost:44303/api/'
+  baseURL: string = 'https://hikeplannerapi.azurewebsites.net/api/';
   userURL: string = this.baseURL + 'users';
   equipmentURL: string = this.baseURL + 'equipments';
   activity: string = this.baseURL + 'activity';
