38a6ccd11c778cfca4ed646077aa5b3cfca11048
sehar-malik <sehar.malik508@gmail.com>
2021-06-14 23:27:55 -0700

    toggle view trips functionality implemented

--- HikePlannerFE/src/app/trips/creatortrips/creatortrips.component.ts
+++ HikePlannerFE/src/app/trips/creatortrips/creatortrips.component.ts
@@ -10,25 +10,39 @@ import { HPApiService } from 'src/app/services/hpapi.service';
   styleUrls: ['./creatortrips.component.css']
 })
 export class CreatortripsComponent implements OnInit {
-  trips: trips[] = [];
+  trips: any[] = [];
   tripInvite : trip[] = [];
+  showTrips: string = 'all';
+  allTrips: any[] = [];
   constructor(private hpApi: HPApiService, private router: Router) { }
   currentUserId = window.sessionStorage.getItem('currentUserId') ?? ''
 
   ngOnInit(): void {
     this.hpApi.GetTripsByCreator(this.currentUserId).then(
-      result => this.trips = result
+      result => {
+        this.trips = result;
+        console.log('only my trips',result);
+      }
+      
       )
       this.hpApi.GetSharedTrips(this.currentUserId).then(
         result => {
-          result.filter(r => r.participants?.filter(p => p.accept == false))
-          this.tripInvite = result;
-         // console.log(currentUserId);
-          console.log("tripInvite -> ",result);
-          console.log("tripInvite -> ",this.tripInvite);
-  
+          result.filter(r => r.participants?.filter(p => p.accept == false));
+          // console.log('shared results',result);
+          this.allTrips = this.allTrips.concat(this.trips,result);
+          console.log('shared results concat',this.allTrips);
         }
-      );
+        );
+      console.log('concatinated trips oninit',this.allTrips);
+      
+    }
+    toggleViewTripList():void {
+      if(this.showTrips == 'all'){
+        this.showTrips = 'onlymine'
+      }else{
+        this.showTrips = 'all';
+      }
+      console.log('show trips flag',this.showTrips);
     }
     TripDetails(tripId: number,activitytrailId: number, activityId: number) : void {
       this.router.navigate(['tripdetails'], {queryParams:{id: tripId, activitytrailId: activitytrailId, activityId: activityId}});
