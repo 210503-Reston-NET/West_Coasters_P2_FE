8f80575f2a5579a5113d8b503bb460dc540e2828
Phoebe <jia.520@outlook.com>
2021-06-17 11:25:54 -0700

    finish up test for checklist

--- HikePlannerFE/src/app/header/header.component.spec.ts
+++ HikePlannerFE/src/app/header/header.component.spec.ts
@@ -1,16 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '@auth0/auth0-angular';
 
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let auth : AuthService
+
+  class MockAuthService {
+
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      imports: [
+        RouterTestingModule,
+        FormsModule
+      ],
+      declarations: [ HeaderComponent ],
+      providers: [
+        {provide: auth, useClass: MockAuthService}
+      ]
     })
     .compileComponents();
+    auth = TestBed.inject(AuthService);
+
   });
 
   beforeEach(() => {
