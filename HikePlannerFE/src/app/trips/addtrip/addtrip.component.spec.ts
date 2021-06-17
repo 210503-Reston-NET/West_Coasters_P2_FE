ed3dd40dde469a8aaae9ab345a8b7701c1f0de10
Phoebe <jia.520@outlook.com>
2021-06-17 13:20:19 -0700

    added some tests for trips

--- HikePlannerFE/src/app/trips/addtrip/addtrip.component.spec.ts
+++ HikePlannerFE/src/app/trips/addtrip/addtrip.component.spec.ts
@@ -1,16 +1,72 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddtripComponent } from './addtrip.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HPApiService } from 'src/app/services/hpapi.service';
+import { MapService } from 'src/app/services/map.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 describe('AddtripComponent', () => {
   let component: AddtripComponent;
   let fixture: ComponentFixture<AddtripComponent>;
+  let router: Router;
+  let service: HPApiService;
+  let route: ActivatedRoute;
+  let mapService: MapService;
+  let bar : MatSnackBar;
+
+  class MockHPApiService
+  {
+    GetChecklistByUserId(id: string):Promise<any> {
+      return new Promise<any>((resolve, reject) => {
+
+      })
+    };
+    GetActivity(id: number):Promise<any> {
+      return new Promise<any>((resolve, reject) => {
+
+      })
+    };
+    AddTrip(trip : any) {}
+  }
+
+  class MockMatSnackBar{
+
+  }
+
+  class MockMapService
+  {
+    GetTrailById(id: number):Promise<any> {
+      return new Promise<any>((resolve, reject) => {
+
+      })
+    };
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddtripComponent ]
+      imports: [
+        RouterTestingModule,
+        FormsModule
+      ],
+      declarations: [ AddtripComponent ],
+      providers: [
+        {provide: MapService, useClass: MockMapService},
+        {provide: HPApiService, useClass: MockHPApiService},
+        {provide: MatSnackBar, useClass: MockMatSnackBar}
+
+      ]
     })
     .compileComponents();
+
+    router = TestBed.inject(Router);
+    service = TestBed.inject(HPApiService);
+    route = TestBed.inject(ActivatedRoute);
+    mapService = TestBed.inject(MapService);
+    bar = TestBed.inject(MatSnackBar);
+
   });
 
   beforeEach(() => {
