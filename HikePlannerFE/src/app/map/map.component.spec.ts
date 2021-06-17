df8a411b3851d9a230ab25770d207a7536ff1b54
Phoebe <jia.520@outlook.com>
2021-06-17 14:26:16 -0700

    completed test for map component

--- HikePlannerFE/src/app/map/map.component.spec.ts
+++ HikePlannerFE/src/app/map/map.component.spec.ts
@@ -1,16 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { RouterTestingModule } from '@angular/router/testing';
 import { MapComponent } from './map.component';
+import { Router } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('MapComponent', () => {
   let component: MapComponent;
   let fixture: ComponentFixture<MapComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MapComponent ]
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([]),
+      ],
+      declarations: [ MapComponent ],
+      //providers: [Router]
     })
     .compileComponents();
+
+    router = TestBed.inject(Router);
+
   });
 
   beforeEach(() => {
